02b02e51ea45583d337ed99c7ee5bf72ad037bee
Daniel Simek <daniel.simek@netrex.cz>
2022-06-08 14:08:53 +0200

    LantronixScaleReader: refactoring and Axis Camera Station events support.

--- lantronixScaleReader/html/index.js
+++ lantronixScaleReader/html/index.js
@@ -2,20 +2,24 @@ $(document).ready(function() {
   $.get('/local/camscripter/package/settings.cgi?package_name=lanScale&action=get', (settings) => {
     console.log(settings);
 
-    $('#cameraUser').val(settings.camera_user);
-    $('#camIP').val(settings.camera_ip);
-    $('#cameraPass').val(settings.camera_pass);
     $('#scaleIP').val(settings.scale_ip);
-    $('#cameraPort').val(settings.camera_port);
     $('#scalePort').val(settings.scale_port);
+    $('#refreshRate').val(settings.refresh_rate);
     $('#serviceId').val(settings.service_id);
+    $('#cameraUser').val(settings.camera_user);
+    $('#cameraIP').val(settings.camera_ip);
+    $('#cameraPass').val(settings.camera_pass);
+    $('#cameraPort').val(settings.camera_port);
     $('#valueFieldName').val(settings.value_field_name);
     $('#unitFieldName').val(settings.unit_field_name);
-    $('#refreshRate').val(settings.refresh_rate);
-    $('#msString').val(settings.milestone_string);
+    $('#acsIP').val(settings.acs_ip);
+    $('#acsWinUser').val(settings.acs_user);
+    $('#acsWinPass').val(settings.acs_pass);
+    $('#acsSourceKey').val(settings.acs_source_key);
     $('#msIP').val(settings.milestone_ip);
     $('#msPort').val(settings.milestone_port);
-    $('#minimumSpan').val(settings.minimum_span);
+    $('#msString').val(settings.milestone_string);
+    $('#msMinimumSpan').val(settings.milestone_minimum_span);
     $('#msSeparators').val(settings.milestone_separator.join(','));
   });
 
@@ -28,22 +32,26 @@ $(document).ready(function() {
 
 function inputChanged() {
   console.log('param changed');
-  var settings = {
-    'camera_user': $('#cameraUser').val(),
-    'camera_ip': $('#camIP').val(),
-    'camera_pass': $('#cameraPass').val(),
-    'camera_port': $('#cameraPort').val(),
+  const settings = {
     'scale_ip': $('#scaleIP').val(),
     'scale_port': parseInt($('#scalePort').val()),
+    'refresh_rate': parseInt($('#refreshRate').val()),
+    'camera_user': $('#cameraUser').val(),
+    'camera_ip': $('#cameraIP').val(),
+    'camera_pass': $('#cameraPass').val(),
+    'camera_port': $('#cameraPort').val(),
     'service_id': $('#serviceId').val(),
     'value_field_name': $('#valueFieldName').val(),
     'unit_field_name': $('#unitFieldName').val(),
+    'acs_ip': $('#acsIP').val(),
+    'acs_user': $('#acsWinUser').val(),
+    'acs_pass': $('#acsWinPass').val(),
+    'acs_source_key': $('#acsSourceKey').val(),
     'milestone_ip': $('#msIP').val(),
-    'milestone_string': $('#msString').val(),
     'milestone_port': parseInt($('#msPort').val()),
-    'refresh_rate': parseInt($('#refreshRate').val()),
-    'minimum_span': parseInt($('#minimumSpan').val()),
-    'milestone_separator': $('#msSeparators').val().split(',').map(function(item){ return parseInt(item);})
+    'milestone_string': $('#msString').val(),
+    'milestone_minimum_span': parseInt($('#msMinimumSpan').val()),
+    'milestone_separator': $('#msSeparators').val().split(',').map((item) => parseInt(item))
   };
   $.post('/local/camscripter/package/settings.cgi?package_name=lanScale&action=set', JSON.stringify(settings), (data) => {});
 }
--- lantronixScaleReader/main.js
+++ lantronixScaleReader/main.js
@@ -1,100 +1,183 @@
 const net = require('net');
 const fs = require('fs');
 const CameraVapix = require('camstreamerlib/CameraVapix');
-const http = require('http');
-const httpRequest = require('camstreamerlib/HTTPRequest');
-const { hostname } = require('os');
+const https = require('https');
 
 let prevWeightData = null;
 let dataBuffer = '';
 
+let cameraVapix = null;
+let milestoneClient = null;
+let milestoneConnected = false;
+let milestoneProtectionPeriod = false;
 
 // Read script configuration
 let settings = null;
 try {
-  let data;
-  if (!process.env.PERSISTENT_DATA_PATH){
-    data = fs.readFileSync('./localdata/settings.json');
-  }else{
-    data = fs.readFileSync(process.env.PERSISTENT_DATA_PATH + 'settings.json');
-  }
-  settings = JSON.parse(data);
+    const data = fs.readFileSync(process.env.PERSISTENT_DATA_PATH + 'settings.json');
+    settings = JSON.parse(data);
 } catch (err) {
-  console.log('No settings file found');
-  return;
+    console.log('No settings file found');
+    process.exit(1);
 }
-let ms_client = new net.Socket();
 
-
-
-// Create camera client for http requests
-let cv = new CameraVapix({
-  'protocol': 'http',
-  'ip': settings.camera_ip,
-  'port': settings.camera_port,
-  'auth': settings.camera_user + ':' + settings.camera_pass,
-});
-let milestone_connected = false;
-let ms_protection_period = false;
-// Connect to electronic scale
-let client = new net.Socket();
-client.connect(settings.scale_port, settings.scale_ip, () => {
-  console.log('Scale connected');
-  if (settings.milestone_ip.length) {
-    try{
-      ms_client.connect(settings.milestone_port, settings.milestone_ip,()=>{
-        milestone_connected = true;
-      });
-    }catch(e){
-      console.log('Milestone Error:' + e);
-    }
-  }
-  setInterval(() => {
-    client.write(Buffer.from('1B700D0A', 'hex'));
-  }, settings.refresh_rate);
-});
-
-client.on('data', (data) => {
-  dataBuffer += Buffer.from(data, 'hex').toString();
-  const messageEnd = dataBuffer.indexOf('\r\n');
-  if (messageEnd == -1) {
-    return;
-  }
-  const weightData = dataBuffer.substring(0, messageEnd);
-  dataBuffer = '';
-
-  if (prevWeightData != weightData) {
-    prevWeightData = weightData;
-    if (milestone_connected && !ms_protection_period){
-      let sep = Buffer.from(settings.milestone_separator);
-      console.log(sep.toString('hex'));
-      ms_client.write(Buffer.from(settings.milestone_string, "ascii") + sep);
-      ms_protection_period = true;
-      setTimeout(()=>{ms_protection_period = false}, settings.minimum_span * 1000)
-    }
-    //Parse weight and unit
-    const weight = prevWeightData.substring(0, 9).trim();
-    const unit = prevWeightData.substring(9).trim();
-    cv.vapixGet('/local/camoverlay/api/textAndBackground.cgi?service_id=' + settings.service_id + '&' +
-      settings.value_field_name + '=' + weight + '&' +
-      settings.unit_field_name + '=' + unit)
-    .then((response) => {
-      //console.log(response);
-    }, function(err) {
-      console.error(err);
+// CamOverlay integration
+const coEnabled = settings.camera_ip && settings.service_id;
+if (coEnabled) {
+    cameraVapix = new CameraVapix({
+        protocol: 'http',
+        ip: settings.camera_ip,
+        port: settings.camera_port,
+        auth: settings.camera_user + ':' + settings.camera_pass,
     });
-  }
+}
+
+// Axis Camera Station integration
+const acsEnabled = settings.acs_ip.length;
+
+// Milestone integration
+const milestoneEnabled = settings.milestone_ip.length;
+
+// Connect to electronic scale
+let scaleClient = new net.Socket();
+scaleClient.connect(settings.scale_port, settings.scale_ip);
+
+scaleClient.on('connect', (data) => {
+    console.log('Scale connected');
+    setInterval(() => {
+        scaleClient.write(Buffer.from('1B700D0A', 'hex'));
+    }, settings.refresh_rate);
 });
 
-client.on('error', (err) => {
-  console.error('Scale connection error: ' + err.toString());
+scaleClient.on('data', (data) => {
+    dataBuffer += Buffer.from(data, 'hex').toString();
+    const messageEnd = dataBuffer.indexOf('\r\n');
+    if (messageEnd == -1) {
+        return;
+    }
+    const weightData = dataBuffer.substring(0, messageEnd);
+    dataBuffer = '';
+
+    if (prevWeightData != weightData) {
+        prevWeightData = weightData;
+
+        //Parse weight and unit
+        const weight = weightData.substring(0, 9).trim();
+        const unit = weightData.substring(9).trim();
+
+        // Show image in CamOverlay service
+        if (coEnabled) {
+            cameraVapix
+                .vapixGet(
+                    '/local/camoverlay/api/customGraphics.cgi?' +
+                        `service_id=${settings.service_id}&` +
+                        `${settings.value_field_name}=${weight}&` +
+                        `${settings.unit_field_name}=${unit}`
+                )
+                .then(
+                    (response) => {
+                        //console.log(response);
+                    },
+                    function (err) {
+                        console.error('CamOverlay error:', err);
+                    }
+                );
+        }
+
+        // Send to Axis Camera Station. Unit is not empty when the weight is stable.
+        if (acsEnabled && weight != 0 && unit.length) {
+            const date = new Date();
+            const year = date.getUTCFullYear();
+            const month = pad(date.getUTCMonth(), 2);
+            const day = pad(date.getUTCDate(), 2);
+            const hours = pad(date.getUTCHours(), 2);
+            const minutes = pad(date.getUTCMinutes(), 2);
+            const seconds = pad(date.getUTCSeconds(), 2);
+            const dateString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+            const event = {
+                addExternalDataRequest: {
+                    occurenceTime: dateString,
+                    source: settings.acs_source_key,
+                    externalDataType: 'LantronixScale',
+                    data: {
+                        timestamp: (Date.now() / 1000).toString(),
+                        weight: weight,
+                        unit,
+                    },
+                },
+            };
+            const eventData = JSON.stringify(event);
+            const req = https.request({
+                method: 'POST',
+                host: settings.acs_ip,
+                port: 55756,
+                path: '/Acs/Api/ExternalDataFacade/AddExternalData',
+                auth: settings.acs_user + ':' + settings.acs_pass,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': eventData.length,
+                },
+                rejectUnauthorized: false,
+            });
+            req.on('error', (err) => {
+                console.error('ACS error:', err);
+            });
+            req.write(eventData);
+            req.end();
+        }
+
+        // Send to Milestone.
+        if (milestoneEnabled) {
+            connectMilestone();
+
+            // Unit is not empty when the weight is stable.
+            if (milestoneConnected && !milestoneProtectionPeriod && weight != 0 && unit.length) {
+                const sep = Buffer.from(settings.milestone_separator);
+                milestoneClient.write(Buffer.from(settings.milestone_string, 'ascii') + sep);
+                milestoneProtectionPeriod = true;
+                setTimeout(() => {
+                    milestoneProtectionPeriod = false;
+                }, settings.milestone_minimum_span * 1000);
+            }
+        }
+    }
 });
 
-client.on('close', () => {
-  console.log('Scale connection closed');
-  process.exit(0);
+scaleClient.on('error', (err) => {
+    console.error('Scale connection error:', err);
+    process.exit(1);
 });
 
-process.on('unhandledRejection', function (error) {
-  console.log('unhandledRejection', error.message);
+scaleClient.on('close', () => {
+    console.log('Scale connection closed');
+    process.exit(0);
 });
+
+function connectMilestone() {
+    if (milestoneClient !== null) {
+        return;
+    }
+
+    milestoneClient = new net.Socket();
+    milestoneClient.connect(settings.milestone_port, settings.milestone_ip);
+    milestoneClient.on('connect', () => {
+        milestoneConnected = true;
+    });
+    milestoneClient.on('error', (err) => {
+        console.log('Milestone connection error:', err);
+        milestoneClient = null;
+        milestoneConnected = false;
+    });
+}
+
+function pad(num, size) {
+    var sign = Math.sign(num) === -1 ? '-' : '';
+    return (
+        sign +
+        new Array(size)
+            .concat([Math.abs(num)])
+            .join('0')
+            .slice(-size)
+    );
+}
