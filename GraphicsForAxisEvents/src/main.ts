3d0a96df3bc00a5c57a11cb1cbab1236d92d6840
VaclavStupka <109210311+VaclavStupka@users.noreply.github.com>
2022-09-22 14:20:54 +0200

    Stupkav/graphics for axis events improvement (#65)
    * Added distinguish between stateful and stateless events.
    
    * Little correction.
    
    * Corrections.
    
    * Better handling of stateful events.
    
    * Better UI.
    
    * Renamed button.
    
    * General handling of stateful events.
    
    * Formated.
    
    * Removed blinking.
    
    * Successfully removed blinking.
    
    * fixup! Successfully removed blinking.
    
    * Enable service only on state change.
    
    * fixup! Enable service only on state change.
    
    * fixup! fixup! Enable service only on state change.

--- GraphicsForAxisEvents/src/main.ts
+++ GraphicsForAxisEvents/src/main.ts
@@ -1,134 +1,163 @@
-import * as fs from "fs";
-import { CameraVapix } from "camstreamerlib/CameraVapix";
-import { CamOverlayAPI } from "camstreamerlib/CamOverlayAPI";
+import * as fs from 'fs';
+import { CameraVapix } from 'camstreamerlib/CameraVapix';
+import { CamOverlayAPI } from 'camstreamerlib/CamOverlayAPI';
 
 type Camera = {
-  IP: string;
-  port: number;
-  user: string;
-  password: string;
+    IP: string;
+    port: number;
+    user: string;
+    password: string;
 };
 
 type Settings = {
-  sourceCamera: Camera;
-  targetCamera: Camera;
-  modem: Camera;
-  events: {
-    eventName: string;
-    serviceID: number;
-    duration: string;
-  }[];
+    sourceCamera: Camera;
+    targetCamera: Camera;
+    modem: Camera;
+    events: {
+        eventName: string;
+        serviceID: number;
+        duration: string;
+        stateName: string;
+        invert: boolean;
+    }[];
 };
 
 type Event = {
-  eventName: string;
-  duration: number;
-  co: CamOverlayAPI;
-  lastTimeout: NodeJS.Timeout;
+    eventName: string;
+    duration: number;
+    serviceID: number;
 };
 
-function onEventMessage(event: Event) {
-  event.co.setEnabled(true);
-  if (event.duration >= 1) {
-    if (event.lastTimeout !== null) {
-      clearTimeout(event.lastTimeout);
+const timeouts: Record<number, NodeJS.Timeout> = {};
+const services: Record<number, CamOverlayAPI> = {};
+
+function onStatelessEvent(event: Event) {
+    services[event.serviceID].setEnabled(true);
+    if (event.duration >= 1) {
+        if (timeouts[event.serviceID] != null) {
+            clearTimeout(timeouts[event.serviceID]);
+        }
+        timeouts[event.serviceID] = setTimeout(() => {
+            services[event.serviceID].setEnabled(false);
+            timeouts[event.serviceID] = null;
+        }, event.duration);
     }
-    event.lastTimeout = setTimeout(() => {
-      event.co.setEnabled(false);
-      event.lastTimeout = null;
-    }, event.duration);
-  }
+}
+
+const lastState: Record<string, boolean> = {};
+function onStatefulEvent(event: Event, state: boolean, invert: boolean) {
+    if (event.duration >= 1) {
+        if (state !== lastState[event.eventName]) {
+            lastState[event.eventName] = state;
+            if (state !== invert) {
+                services[event.serviceID].setEnabled(true);
+                if (timeouts[event.serviceID] !== null) {
+                    clearTimeout(timeouts[event.serviceID]);
+                }
+                timeouts[event.serviceID] = setTimeout(() => {
+                    services[event.serviceID].setEnabled(false);
+                    timeouts[event.serviceID] = null;
+                }, event.duration);
+            } else if (timeouts[event.serviceID] != null) {
+                services[event.serviceID].setEnabled(false);
+                clearTimeout(timeouts[event.serviceID]);
+            }
+        }
+    } else {
+        services[event.serviceID].setEnabled(state !== invert);
+    }
 }
 
 function getSettings() {
-  try {
-    const path = "./localdata/"; //process.env.PERSISTENT_DATA_PATH;
-    const data = fs.readFileSync(path + "settings.json");
-    return JSON.parse(data.toString());
-  } catch (error) {
-    console.log("Error with Settings file: ", error);
-    process.exit(1);
-  }
+    try {
+        const path = process.env.PERSISTENT_DATA_PATH;
+        const data = fs.readFileSync(path + 'settings.json');
+        return JSON.parse(data.toString());
+    } catch (error) {
+        console.log('Error with Settings file: ', error);
+        process.exit(1);
+    }
 }
 
 async function prepareCamOverlay(settings: Settings) {
-  const cos: Record<number, CamOverlayAPI> = {};
-  for (let event of settings.events) {
-    const options = {
-      ip: settings.targetCamera.IP,
-      port: settings.targetCamera.port,
-      auth: `${settings.targetCamera.user}:${settings.targetCamera.password}`,
-      serviceID: event.serviceID,
-    };
-    try {
-      const co = new CamOverlayAPI(options);
-      await co.connect();
-      await co.setEnabled(false);
-      cos[event.serviceID] = co;
-    } catch (error) {
-      console.log(
-        `Cannot connect to CamOverlay service with ID ${event.serviceID} (${error})`
-      );
-      process.exit(1);
+    for (let event of settings.events) {
+        const options = {
+            ip: settings.targetCamera.IP,
+            port: settings.targetCamera.port,
+            auth: `${settings.targetCamera.user}:${settings.targetCamera.password}`,
+            serviceID: event.serviceID,
+        };
+        try {
+            const co = new CamOverlayAPI(options);
+            await co.connect();
+            await co.setEnabled(false);
+            services[event.serviceID] = co;
+        } catch (error) {
+            console.log(`Cannot connect to CamOverlay service with ID ${event.serviceID} (${error})`);
+            process.exit(1);
+        }
     }
-  }
-  return cos;
 }
 
-async function subscribeEventMessages(
-  settings: Settings,
-  cos: Record<number, CamOverlayAPI>
-) {
-  const options = {
-    protocol: "http",
-    ip: settings.targetCamera.IP,
-    port: settings.targetCamera.port,
-    auth: `${settings.targetCamera.user}:${settings.targetCamera.password}`,
-  };
+function isStateful(eventData, stateName: string) {
+    return eventData[stateName] !== undefined;
+}
 
-  const cv = new CameraVapix(options);
-  cv.on("eventsDisconnect", (error) => {
-    if (error == undefined) {
-      console.log("Websocket disconnected.");
-    } else {
-      console.log("Websocket error: ", error);
-    }
-    process.exit(1);
-  });
+async function subscribeEventMessages(settings: Settings) {
+    const options = {
+        protocol: 'http',
+        ip: settings.targetCamera.IP,
+        port: settings.targetCamera.port,
+        auth: `${settings.targetCamera.user}:${settings.targetCamera.password}`,
+    };
 
-  for (let event of settings.events) {
-    const time = event.duration.split(":");
-    const hours = Number.parseInt(time[0]);
-    const minutes = Number.parseInt(time[1]);
-    const seconds = Number.parseInt(time[2]);
-
-    let duration = (3600 * hours + 60 * minutes + seconds) * 1000;
-    if (Number.isNaN(duration)) {
-      duration = 0;
-    }
-
-    cv.on(event.eventName, () => {
-      const e = {
-        eventName: event.eventName,
-        duration: duration,
-        co: cos[event.serviceID],
-        lastTimeout: null,
-      };
-      onEventMessage(e);
+    const cv = new CameraVapix(options);
+    cv.on('eventsDisconnect', (error) => {
+        if (error == undefined) {
+            console.log('Websocket disconnected.');
+        } else {
+            console.log('Websocket error: ', error);
+        }
+        process.exit(1);
     });
-  }
-  cv.eventsConnect("websocket");
+
+    for (let event of settings.events) {
+        const time = event.duration.split(':');
+        const hours = Number.parseInt(time[0]);
+        const minutes = Number.parseInt(time[1]);
+        const seconds = Number.parseInt(time[2]);
+
+        let duration = (3600 * hours + 60 * minutes + seconds) * 1000;
+        if (Number.isNaN(duration)) {
+            duration = 0;
+        }
+
+        cv.on(event.eventName, (data) => {
+            const eventData = data.params.notification.message.data;
+            const e = {
+                eventName: event.eventName,
+                duration: duration,
+                serviceID: event.serviceID,
+            };
+            if (isStateful(eventData, event.stateName)) {
+                onStatefulEvent(e, eventData[event.stateName] === '1', event.invert);
+            } else {
+                onStatelessEvent(e);
+            }
+        });
+    }
+    cv.eventsConnect('websocket');
 }
 
 async function main() {
-  const settings = getSettings();
-  const cos = await prepareCamOverlay(settings);
-  subscribeEventMessages(settings, cos);
+    const settings = getSettings();
+    await prepareCamOverlay(settings);
+    subscribeEventMessages(settings);
 }
 
-process.on("unhandledRejection", (reason) => {
-  console.log(reason);
-  process.exit(1);
+process.on('unhandledRejection', (reason) => {
+    console.log(reason);
+    process.exit(1);
 });
 
 main();
