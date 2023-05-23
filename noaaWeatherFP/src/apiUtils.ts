3fe7da21f020cca5a89578b382c015f8eefab4cc
Jakub Pejcha <jakub.pejcha@netrex.cz>
2023-05-23 08:38:04 +0200

    Adding commas into next tides string

--- noaaWeatherFP/src/apiUtils.ts
+++ noaaWeatherFP/src/apiUtils.ts
@@ -81,15 +81,15 @@ export const prepareAllDataFetch = (queryParams: TApiQueryParams): TDataPromises
         return obj;
     }, {} as TDataPromises);
 
-export const parseNextTideData = (predictions: TNextTideApiData['predictions']) =>
-    predictions
-        .reduce((acc, curr) => {
-            const timeString = new Date(curr.t).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-            const position = curr.type === 'H' ? 'high' : 'low';
-            acc += `${timeString} ${position} ${curr.v} `;
-            return acc;
-        }, '')
-        .trim();
+export const parseNextTideData = (predictions: TNextTideApiData['predictions']) => {
+    const str = predictions.reduce((acc, curr) => {
+        const timeString = new Date(curr.t).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+        const position = curr.type === 'H' ? 'high' : 'low';
+        acc += `${timeString} ${position} ${curr.v}, `;
+        return acc;
+    }, '');
+    return str.substring(0, str.length - 2); // remove last ', '
+};
 
 const fetchApiData = async (urlEndpoint: TEndpoints, queryParams: TApiQueryParams) => {
     return new Promise<string>((resolve, reject) => {
