38879c77d92beaa90c2935021c5f5d89bfad77bb
Daniel Simek <daniel.simek@netrex.cz>
2023-06-06 16:49:07 +0200

    Axis camera events added to temper1fSensor script.

--- Raspberry/temper1fSensor/html/index.js
+++ Raspberry/temper1fSensor/html/index.js
@@ -18,6 +18,15 @@ $(document).ready(function () {
         $('#acsConditionOperator').val(settings.acs_condition_operator ?? 1);
         $('#acsConditionValue').val(settings.acs_condition_value ?? 10);
         $('#acsRepeatAfter').val(settings.acs_repeat_after ?? 30);
+        $('#eventCameraProtocol').val(settings.event_camera_protocol ?? 'http');
+        $('#eventCameraIP').val(settings.event_camera_ip);
+        $('#eventCameraPort').val(settings.event_camera_port ?? 80);
+        $('#eventCameraUser').val(settings.event_camera_user ?? 'root');
+        $('#eventCameraPass').val(settings.event_camera_pass);
+        $('#eventConditionDelay').val(settings.event_condition_delay ?? 0);
+        $('#eventConditionOperator').val(settings.event_condition_operator ?? 1);
+        $('#eventConditionValue').val(settings.event_condition_value ?? 10);
+
         unit = settings.unit;
         unitChanged();
     });
@@ -42,6 +51,7 @@ function radioClickedCallback() {
 
 function unitChanged() {
     $('#acsConditionUnit').text(String.fromCharCode(176) + unit.toLocaleUpperCase());
+    $('#eventConditionUnit').text(String.fromCharCode(176) + unit.toLocaleUpperCase());
     inputChanged();
 }
 
@@ -64,6 +74,14 @@ function inputChanged() {
         acs_condition_operator: parseInt($('#acsConditionOperator').val()),
         acs_condition_value: parseFloat($('#acsConditionValue').val()),
         acs_repeat_after: parseInt($('#acsRepeatAfter').val()),
+        event_camera_protocol: $('#eventCameraProtocol').val(),
+        event_camera_ip: $('#eventCameraIP').val(),
+        event_camera_port: $('#eventCameraPort').val(),
+        event_camera_user: $('#eventCameraUser').val(),
+        event_camera_pass: $('#eventCameraPass').val(),
+        event_condition_delay: parseInt($('#eventConditionDelay').val()),
+        event_condition_operator: parseInt($('#eventConditionOperator').val()),
+        event_condition_value: parseFloat($('#eventConditionValue').val()),
     };
     $.post(
         '/local/camscripter/package/settings.cgi?package_name=temper1fSensor&action=set',
