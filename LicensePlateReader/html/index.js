09bf0ca4d3cff539eabd7d04941aecc35bd68179
basvas j.k.j <basvas@seznam.cz>
2023-07-10 11:47:19 +0200

    Handling with the select box in index.js

--- LicensePlateReader/html/index.js
+++ LicensePlateReader/html/index.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     $.get('/local/camscripter/package/settings.cgi?package_name=LicensePlateReader&action=get', function (settings) {
         $('#scIP').val(settings.sourceCamera.IP);
+        $('#scProtocol').val(settings.sourceCamera.protocol);
         $('#scPort').val(settings.sourceCamera.port);
         $('#scUser').val(settings.sourceCamera.user);
         $('#scPassword').val(settings.sourceCamera.password);
 
         $('#tcIP').val(settings.targetCamera.IP);
+        $('#tcProtocol').val(settings.targetCamera.protocol);
         $('#tcPort').val(settings.targetCamera.port);
         $('#tcUser').val(settings.targetCamera.user);
         $('#tcPassword').val(settings.targetCamera.password);
@@ -17,7 +19,9 @@ $(document).ready(function () {
         $('#tsFieldName').val(settings.tsFieldName);
         $('#visibilityTime').val(settings.visibilityTime);
     });
-
+    
+    $("#scProtocol").change(() => protocolChanged("sc"));
+    $("#tcProtocol").change(() => protocolChanged("tc"));
     $(".form-control").change(inputChanged);
 
     $(".myForm").submit(function () {
@@ -25,12 +29,24 @@ $(document).ready(function () {
     });
 });
 
+function protocolChanged(prefix) {
+    if ($(`#${prefix}Protocol`).val() === "http")
+    {
+        $(`#${prefix}Port`).val(80);
+    }
+    else
+    {
+        $(`#${prefix}Port`).val(443);
+    }
+}
+
 function inputChanged() {
     let settings =
     {
         sourceCamera:
         {
             IP: $('#scIP').val(),
+            protocol: $('#scProtocol').val(),
             port: $('#scPort').val(),
             user: $('#scUser').val(),
             password: $('#scPassword').val()
@@ -39,6 +55,7 @@ function inputChanged() {
         targetCamera:
         {
             IP: $('#tcIP').val(),
+            protocol: $('#tcProtocol').val(),
             port: $('#tcPort').val(),
             user: $('#tcUser').val(),
             password: $('#tcPassword').val()
