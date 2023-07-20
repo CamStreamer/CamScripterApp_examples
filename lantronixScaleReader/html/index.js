d3425e9222ee859073b9ba488c8c23524e7dfdd3
Václav Stupka <vaclav.stupka@netrex.cz>
2023-07-20 09:31:59 +0200

    Lantronix: added event settings to UI.

--- lantronixScaleReader/html/index.js
+++ lantronixScaleReader/html/index.js
@@ -5,19 +5,31 @@ $(document).ready(function () {
         $('#scaleIP').val(settings.scale_ip);
         $('#scalePort').val(settings.scale_port);
         $('#refreshRate').val(settings.refresh_rate);
-        $('#serviceId').val(settings.service_id);
+
         $('#cameraProtocol').val(settings.camera_protocol);
         $('#cameraIP').val(settings.camera_ip);
         $('#cameraPort').val(settings.camera_port);
         $('#cameraUser').val(settings.camera_user);
         $('#cameraPass').val(settings.camera_pass);
+        $('#serviceId').val(settings.service_id);
         $('#valueFieldName').val(settings.value_field_name);
         $('#unitFieldName').val(settings.unit_field_name);
+
+        $('#eventCameraProtocol').val(settings.camera_protocol);
+        $('#eventCameraIP').val(settings.camera_ip);
+        $('#eventCameraPort').val(settings.camera_port);
+        $('#eventCameraUser').val(settings.camera_user);
+        $('#eventCameraPass').val(settings.camera_pass);
+        $('#eventConditionDelay').val(settings.event_condition_delay ?? 0);
+        $('#eventConditionOperator').val(settings.event_condition_operator ?? 1);
+        $('#eventConditionValue').val(settings.event_condition_value ?? 10);
+
         $('#acsProtocol').val(settings.acs_protocol);
         $('#acsIP').val(settings.acs_ip);
         $('#acsWinUser').val(settings.acs_user);
         $('#acsWinPass').val(settings.acs_pass);
         $('#acsSourceKey').val(settings.acs_source_key);
+
         $('#msIP').val(settings.milestone_ip);
         $('#msPort').val(settings.milestone_port);
         $('#msString').val(settings.milestone_string);
@@ -25,7 +37,8 @@ $(document).ready(function () {
         $('#msSeparators').val(settings.milestone_separator.join(','));
     });
 
-    $('#cameraProtocol').change(protocolChanged);
+    $('#cameraProtocol').change(() => protocolChanged("camera"));
+    $('#eventCameraProtocol').change(() => protocolChanged("eventCamera"));
     $('.form-control').change(inputChanged);
 
     $('.myForm').submit(() => {
@@ -33,9 +46,9 @@ $(document).ready(function () {
     });
 });
 
-function protocolChanged() {
-    const port = $('#cameraProtocol').val() === 'http' ? 80 : 443;
-    $('#cameraPort').val(port);
+function protocolChanged(prefix) {
+    const port = $(`#${prefix}Protocol`).val() === 'http' ? 80 : 443;
+    $(`#${prefix}Port`).val(port);
 }
 
 function inputChanged() {
@@ -44,6 +57,7 @@ function inputChanged() {
         scale_ip: $('#scaleIP').val(),
         scale_port: parseInt($('#scalePort').val()),
         refresh_rate: parseInt($('#refreshRate').val()),
+
         camera_protocol: $('#cameraProtocol').val(),
         camera_ip: $('#cameraIP').val(),
         camera_port: $('#cameraPort').val(),
@@ -52,11 +66,22 @@ function inputChanged() {
         service_id: $('#serviceId').val(),
         value_field_name: $('#valueFieldName').val(),
         unit_field_name: $('#unitFieldName').val(),
+
+        event_camera_protocol: $('#eventCameraProtocol').val(),
+        event_camera_ip: $('#eventCameraIP').val(),
+        event_camera_port: $('#eventCameraPort').val(),
+        event_camera_user: $('#eventCameraUser').val(),
+        event_camera_pass: $('#eventCameraPass').val(),
+        event_condition_delay: $('#eventConditionDelay').val(),
+        event_condition_operator: $('#eventConditionOperator').val(),
+        event_condition_value: $('#eventConditionValue').val(),
+
         acs_protocol: $('#acsProtocol').val(),
         acs_ip: $('#acsIP').val(),
         acs_user: $('#acsWinUser').val(),
         acs_pass: $('#acsWinPass').val(),
         acs_source_key: $('#acsSourceKey').val(),
+
         milestone_ip: $('#msIP').val(),
         milestone_port: parseInt($('#msPort').val()),
         milestone_string: $('#msString').val(),
@@ -64,11 +89,8 @@ function inputChanged() {
         milestone_separator: $('#msSeparators')
             .val()
             .split(',')
-            .map((item) => parseInt(item)),
+            .map((item) => parseInt(item))
     };
-    $.post(
-        '/local/camscripter/package/settings.cgi?package_name=lanScale&action=set',
-        JSON.stringify(settings),
-        (data) => {}
-    );
+
+    $.post('/local/camscripter/package/settings.cgi?package_name=lanScale&action=set', JSON.stringify(settings));
 }
