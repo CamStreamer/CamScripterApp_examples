d1e0592e1076ae4936d878f1d24768c18fc90b28
Jan Tichavsky <jan.tichavsky@netrex.cz>
2023-11-15 16:48:53 +0100

    laod settings

--- barcodeReaderAndUploader/src/main.ts
+++ barcodeReaderAndUploader/src/main.ts
@@ -1,1 +1,12 @@
+import * as fs from 'fs';
 
+let settings = null;
+try {
+    const data = fs.readFileSync(process.env.PERSISTENT_DATA_PATH + 'settings.json');
+    settings = JSON.parse(data.toString());
+} catch (err) {
+    console.log('No settings file found');
+    process.exit(1);
+}
+
+console.log('Hello world');
