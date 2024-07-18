bb0707723905cd89b8e2f38ae2160d03c766d383
Václav Stupka <vaclav.stupka@netrex.cz>
2024-07-18 15:21:30 +0200

    FE: react configuration

---
+++ LutronLuxmeterIntegration/react/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    extends: ['@camstreamer/eslint-config'],
+    root: true,
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        "@typescript-eslint/strict-boolean-expressions": "off"
+    }
+};
