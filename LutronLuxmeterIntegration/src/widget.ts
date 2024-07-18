c6e40a1cefd9ea14ace9ab2e2f2688c3dd2bfd4d
Václav Stupka <vaclav.stupka@netrex.cz>
2024-07-18 14:41:10 +0200

    Update widget

--- LutronLuxmeterIntegration/src/widget.ts
+++ LutronLuxmeterIntegration/src/widget.ts
@@ -1,23 +1,33 @@
 import { CamOverlayDrawingOptions } from 'camstreamerlib/CamOverlayDrawingAPI';
 import { Painter, PainterOptions, Frame, ResourceManager } from 'camstreamerlib/CamOverlayPainter/Painter';
 
-const borderWidth = 5;
 const imageWidth = 325;
-const luxmeterHeight = 244;
+const luxmeterHeight = 254;
 const camstreamerHeight = 92;
+
+function toString(value: number): string {
+    if (value === 0 || isNaN(value)) {
+        return 'ERROR';
+    } else {
+        const text = value.toPrecision(4);
+        return text.substring(0, 6);
+    }
+}
+
 export class Widget {
     private rm = new ResourceManager();
     private background: Painter;
     private camstreamer: Frame;
     private luxmeter: Frame;
-    private border: Frame;
     private value: Frame;
+    private scale: number;
 
     public constructor(
-        opt: Omit<PainterOptions, 'width' | 'height'>,
-        coOpt: CamOverlayDrawingOptions,
-        private scale: number
+        opt: Omit<PainterOptions, 'width' | 'height'> & { scale: number },
+        coOpt: CamOverlayDrawingOptions
     ) {
+        this.scale = opt.scale;
+        this.rm.registerFont('Digital', 'digital_font.ttf');
         this.rm.registerImage('Luxmeter', 'luxmeter.png');
         this.rm.registerImage('CamStreamer', 'camstreamer.png');
 
@@ -32,11 +42,11 @@ export class Widget {
         );
         this.luxmeter = new Frame(
             {
-                x: borderWidth,
-                y: borderWidth,
-                width: imageWidth - 2 * borderWidth,
-                height: luxmeterHeight - 2 * borderWidth,
-                bgColor: [205 / 256, 206 / 256, 196 / 256, 1],
+                x: 0,
+                y: 0,
+                width: imageWidth,
+                height: luxmeterHeight,
+                bgImage: 'Luxmeter',
             },
             this.rm
         );
@@ -53,22 +63,13 @@ export class Widget {
             },
             this.rm
         );
-        this.border = new Frame(
-            {
-                x: 0,
-                y: 0,
-                width: imageWidth,
-                height: luxmeterHeight,
-                bgColor: [27 / 256, 60 / 256, 112 / 256, 1],
-            },
-            this.rm
-        );
 
-        this.background.insert(this.border, this.luxmeter, this.value, this.camstreamer);
+        this.value.setFont('Digital');
+        this.background.insert(this.luxmeter, this.value, this.camstreamer);
         void this.background.connect();
     }
     public display(value: number): Promise<void> {
-        this.value.setText(value.toString(), 'A_CENTER', 'TFM_SCALE', [0, 0, 0]);
+        this.value.setText(toString(value), 'A_CENTER', 'TFM_SCALE', [0, 0, 0]);
         return this.background.display(this.scale);
     }
 }
