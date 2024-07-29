06a0dd32dca1b24787414f7b27c72f256d09680c
Václav Stupka <vaclav.stupka@netrex.cz>
2024-07-29 10:53:56 +0200

    BE: widget supports displaying on more cameras

--- LutronLuxmeterIntegration/src/widget.ts
+++ LutronLuxmeterIntegration/src/widget.ts
@@ -13,9 +13,21 @@ function toString(value: number): string {
         return text.substring(0, 6);
     }
 }
+function initialisePainter(opt: PainterOptions, coOpt: CamOverlayDrawingOptions, ...frames: Frame[]) {
+    const p = new Painter(opt, coOpt);
+
+    p.registerFont('Digital', 'digital_font.ttf');
+    p.registerImage('Luxmeter', 'luxmeter.png');
+    p.registerImage('CamStreamer', 'camstreamer.png');
+
+    p.insert(...frames);
+    void p.connect();
+
+    return p;
+}
 
 export class Widget {
-    private background: Painter;
+    private painters: Painter[];
     private camstreamer: Frame;
     private luxmeter: Frame;
     private value: Frame;
@@ -23,17 +35,8 @@ export class Widget {
 
     public constructor(
         opt: Omit<PainterOptions, 'width' | 'height'> & { scale: number },
-        coOpt: CamOverlayDrawingOptions
+        cameras: CamOverlayDrawingOptions[]
     ) {
-        this.scale = opt.scale;
-        this.background = new Painter(
-            {
-                ...opt,
-                width: imageWidth,
-                height: luxmeterHeight + camstreamerHeight,
-            },
-            coOpt
-        );
         this.luxmeter = new Frame({
             x: 0,
             y: 0,
@@ -55,16 +58,34 @@ export class Widget {
             height: luxmeterHeight * (3 / 4),
         });
 
-        this.background.registerFont('Digital', 'digital_font.ttf');
-        this.background.registerImage('Luxmeter', 'luxmeter.png');
-        this.background.registerImage('CamStreamer', 'camstreamer.png');
+        this.scale = opt.scale;
+        this.painters = [];
+        for (const coOpt of cameras) {
+            this.painters.push(
+                initialisePainter(
+                    {
+                        ...opt,
+                        width: imageWidth,
+                        height: luxmeterHeight + camstreamerHeight,
+                    },
+                    coOpt,
+                    this.luxmeter,
+                    this.value,
+                    this.camstreamer
+                )
+            );
+        }
 
         this.value.setFont('Digital');
-        this.background.insert(this.luxmeter, this.value, this.camstreamer);
-        void this.background.connect();
     }
-    public display(value: number): Promise<void> {
+    public async display(value: number): Promise<void> {
         this.value.setText(toString(value), 'A_CENTER', 'TFM_SCALE', [0, 0, 0]);
-        return this.background.display(this.scale);
+
+        const promises = new Array<Promise<void>>();
+
+        for (const p of this.painters) {
+            promises.push(p.display(this.scale));
+        }
+        await Promise.all(promises);
     }
 }
