ee6214cf32d6cb05a9ae73ae4bdacde4125563e8
Václav Stupka <vaclav.stupka@netrex.cz>
2024-08-01 10:45:54 +0200

    BE: continuous reading

--- LutronLuxmeterIntegration/src/reader.ts
+++ LutronLuxmeterIntegration/src/reader.ts
@@ -4,16 +4,43 @@ import { SerialPort } from 'serialport';
 export type TResult = { value: number; unit: string };
 
 const bufferSize = 16;
+const STX = 2;
+const LF = 10;
 
 export class LuxMeterReader {
-    private path = '';
+    private device: fs.FileHandle | undefined;
+    private last = Buffer.alloc(0);
+
+    private constructor() {}
+    private async findLuxMeter(): Promise<void> {
+        const list = await SerialPort.list();
+
+        for (const port of list) {
+            this.device = await fs.open(port.path, 'r');
+            const buf = await this.read();
+
+            if (this.validResponse(buf)) {
+                this.last = buf;
+                void this.readLoop();
+                return;
+            } else {
+                void this.device.close();
+            }
+        }
+
+        throw new Error('No Lux meter found.');
+    }
+    static async connect() {
+        const lmr = new LuxMeterReader();
+        await lmr.findLuxMeter();
+        return lmr;
+    }
+
     private validResponse(buf: Buffer): boolean {
-        const STX = 2;
-        const LF = 10;
         const zeroCode = '0'.charCodeAt(0);
         const nineCode = '9'.charCodeAt(0);
 
-        if (buf.length !== bufferSize || buf[0] !== STX || buf.at(-1) !== LF) {
+        if (buf[0] !== STX || buf.at(-1) !== LF) {
             return false;
         }
         for (let f = 1; f < bufferSize - 1; f += 1) {
@@ -24,39 +51,23 @@ export class LuxMeterReader {
 
         return true;
     }
-    private async findLuxMeter(): Promise<void> {
-        const list = await SerialPort.list();
-
-        for (const port of list) {
-            const buf = await this.read(port.path);
-
-            if (this.validResponse(buf)) {
-                this.path = port.path;
-                return;
-            }
+    private async readLoop() {
+        for (;;) {
+            this.last = await this.read();
         }
-
-        throw new Error('No Lux meter found.');
     }
+    async read(): Promise<Buffer> {
+        let buf = Buffer.alloc(0);
+        const byte = Buffer.alloc(1);
+        do {
+            await this.device!.read(byte, 0, 1, null);
+            buf = Buffer.concat([buf, byte]);
+        } while (byte.at(0) !== LF);
 
-    private constructor() {}
-    static async connect() {
-        const lmr = new LuxMeterReader();
-        await lmr.findLuxMeter();
-        return lmr;
-    }
-
-    async read(path?: string): Promise<Buffer> {
-        path ??= this.path;
-        const buf = Buffer.alloc(bufferSize);
-
-        const device = await fs.open(path ?? '', 'r');
-        await device.read(buf, 0, bufferSize);
-        void device.close();
         return buf;
     }
-    async readParsed(): Promise<TResult> {
-        const s = (await this.read()).toString();
+    readParsed(): TResult {
+        const s = this.last.toString();
         const value = Number.parseInt(s.substring(7, 15));
         const decimalPointPosition = Number.parseInt(s[6]);
         const unit = Number.parseInt(s.substring(3, 5));
