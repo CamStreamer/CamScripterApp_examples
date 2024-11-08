cd3f18477e7ab0ee3d3ee983835b5d999b09d5e0
Helena Beranova <helena.beranova@netrex.cz>
2024-11-08 17:00:52 +0100

    created appInfo file to use as template and implemented it

---
+++ template/react/src/appInfo.ts
@@ -0,0 +1,6 @@
+export const appInfo = {
+  //adjust application accodingly
+  name: "Papago",
+  packageName: "papago_temperature",
+  headline: "PAPAGO Temperature (IP device)",
+};
--- template/react/src/models/schema.ts
+++ template/react/src/models/schema.ts
@@ -1,29 +1,37 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const connectionParams = {
-    protocol: z.union([z.literal('http'), z.literal('https'), z.literal('https_insecure')]),
-    ip: z.string(),
-    port: z.number(),
-    user: z.string(),
-    pass: z.string(),
+  protocol: z.union([
+    z.literal("http"),
+    z.literal("https"),
+    z.literal("https_insecure"),
+  ]),
+  ip: z.string(),
+  port: z.number(),
+  user: z.string(),
+  pass: z.string(),
 };
 export const cameraSchema = z.object({
-    ...connectionParams,
-    serviceID: z.number(),
-    fieldName: z.string(),
+  ...connectionParams,
+  serviceID: z.number(),
+  fieldName: z.string(),
 });
-export const papagoSchema = z.object({
-    protocol: z.union([z.literal('http'), z.literal('https'), z.literal('https_insecure')]),
-    ip: z.string(),
-    port: z.number(),
-    portID: z.string(),
-    updateFrequency: z.number(),
+export const applicationSchema = z.object({
+  protocol: z.union([
+    z.literal("http"),
+    z.literal("https"),
+    z.literal("https_insecure"),
+  ]),
+  ip: z.string(),
+  port: z.number(),
+  portID: z.string(),
+  updateFrequency: z.number(),
 });
 export const settingsSchema = z.object({
-    camera: cameraSchema,
-    papago: papagoSchema,
+  camera: cameraSchema,
+  application: applicationSchema,
 });
 
 export type TCamera = z.infer<typeof cameraSchema>;
-export type TPapago = z.infer<typeof papagoSchema>;
+export type TPapago = z.infer<typeof applicationSchema>;
 export type TSettings = z.infer<typeof settingsSchema>;
