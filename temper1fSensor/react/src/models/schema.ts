ca3971df7a917bcc58f965153e10fa0819f42b03
Helena Beranova <helena.beranova@netrex.cz>
2024-11-12 15:03:16 +0100

    schema and mock values update

--- temper1fSensor/react/src/models/schema.ts
+++ temper1fSensor/react/src/models/schema.ts
@@ -1,22 +1,22 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const applicationSchema = z.object({
   camera_protocol: z.union([
-    z.literal("http"),
-    z.literal("https"),
-    z.literal("https_insecure"),
+    z.literal('http'),
+    z.literal('https'),
+    z.literal('https_insecure'),
   ]),
   camera_ip: z.string(),
   camera_port: z.number(),
   camera_user: z.string(),
   camera_pass: z.string(),
-  unit: z.string(),
+  unit: z.union([z.literal('c'), z.literal('f')]),
   service_id: z.number(),
   field_name: z.string(),
   acs_protocol: z.union([
-    z.literal("http"),
-    z.literal("https"),
-    z.literal("https_insecure"),
+    z.literal('http'),
+    z.literal('https'),
+    z.literal('https_insecure'),
   ]),
   acs_ip: z.string(),
   acs_port: z.number(),
@@ -28,17 +28,23 @@ export const applicationSchema = z.object({
   acs_condition_value: z.number(),
   acs_repeat_after: z.number(),
   event_camera_protocol: z.union([
-    z.literal("http"),
-    z.literal("https"),
-    z.literal("https_insecure"),
+    z.literal('http'),
+    z.literal('https'),
+    z.literal('https_insecure'),
   ]),
   event_camera_ip: z.string(),
   event_camera_port: z.number(),
   event_camera_user: z.string(),
   event_camera_pass: z.string(),
-  event_condition_delay: z.number(),
-  event_condition_operator: z.number(),
-  event_condition_value: z.number()
+  event_condition_delay: z.number().int(),
+  event_condition_operator: z.number().int().min(0).max(4),
+  event_condition_value: z.number(),
+
+  event_view_areas: z.array(z.number()),
+  event_active: z.boolean(),
+  event_repeat_after: z.number(),
+  view_areas: z.array(z.number()),
+  acs_active: z.boolean(),
 });
 
 export type TAppSchema = z.infer<typeof applicationSchema>;
