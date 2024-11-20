bd4ca5a5b404649c931af40f07b68fa1f79c60df
Helena Beranova <helena.beranova@netrex.cz>
2024-11-20 15:30:32 +0100

    add and implement base get and post urls

--- template/react/src/App.tsx
+++ template/react/src/App.tsx
@@ -1,101 +1,99 @@
-import { appInfo } from "./appInfo";
-import { useState } from "react";
-import { Box } from "@mui/material";
-import Container from "@mui/material/Container";
-import styled from "@mui/material/styles/styled";
-import CssBaseline from "@mui/material/CssBaseline";
+import { appInfo } from './appInfo';
+import { useState } from 'react';
+import { Box, Container, CssBaseline } from '@mui/material';
+import styled from '@mui/material/styles/styled';
 
-import { Nav } from "./components/Nav";
-import { ContainerLoader } from "./components/ContainerLoader";
-import { TSettings, settingsSchema } from "./models/schema";
-import { useInitializeOnMount } from "./hooks/useInitializeOnMount";
-import { ZodError } from "zod";
-import { useSnackbar } from "./hooks/Snackbar";
-import { Headline } from "./components/Title";
-import { FormWrapper } from "./FormWrapper";
-import { mockedSettings } from "./models/mock";
+import { Nav } from './components/Nav';
+import { ContainerLoader } from './components/ContainerLoader';
+import { TSettings, settingsSchema } from './models/schema';
+import { useInitializeOnMount } from './hooks/useInitializeOnMount';
+import { ZodError } from 'zod';
+import { useSnackbar } from './hooks/useSnackbar';
+import { Headline } from './components/Title';
+import { FormWrapper } from './FormWrapper';
+import { mockedSettings } from './models/mock';
 
 document.title = appInfo.title;
 
 export const App = () => {
-  const { displaySnackbar } = useSnackbar();
+    const { displaySnackbar } = useSnackbar();
 
-  const [defaultValues, setDefaultValues] = useState<TSettings | null>(null);
+    const [defaultValues, setDefaultValues] = useState<TSettings | null>(null);
 
-  useInitializeOnMount(async () => {
-    let response: Response;
-    try {
-      let url = BASE_URL;
-      if (process.env!.NODE_ENV === "development") {
-        url = "http://localhost:52520" + url;
-      }
+    useInitializeOnMount(async () => {
+        let response: Response;
+        try {
+            let url = BASE_URL;
+            if (process.env!.NODE_ENV === 'development') {
+                url = 'http://localhost:52520' + url;
+            }
 
-      response = await fetch(url);
-      const parsedData = settingsSchema.parse(await response.json());
-      setDefaultValues(parsedData);
-    } catch (e) {
-      if (e instanceof ZodError) {
-        displaySnackbar({
-          type: "error",
-          message: "Data from server do not match expected data",
-        });
-      } else {
-        displaySnackbar({
-          type: "error",
-          message: "Error fetching form data.",
-        });
-      }
+            response = await fetch(url);
+            const parsedData = settingsSchema.parse(await response.json());
+            setDefaultValues(parsedData);
+        } catch (e) {
+            if (e instanceof ZodError) {
+                displaySnackbar({
+                    type: 'error',
+                    message: 'Data from server do not match expected data',
+                });
+            } else {
+                displaySnackbar({
+                    type: 'error',
+                    message: 'Error fetching form data.',
+                });
+            }
 
-      console.error("Error while fetching default values: ", e);
-      setDefaultValues(mockedSettings);
-    }
-  });
+            console.error('Error while fetching default values: ', e);
+            setDefaultValues(mockedSettings);
+        }
+    });
 
-  return (
-    <>
-      <Nav />
-      <StyledContainer>
-        <CssBaseline />
-        <StyledContentWrapper>
-          <StyledHeader>
-            <Headline text={appInfo.headline} />
-          </StyledHeader>
-          {defaultValues ? (
-            <FormWrapper defaultValues={defaultValues} />
-          ) : (
-            <ContainerLoader size={80} infoText="Fetching settings..." />
-          )}
-        </StyledContentWrapper>
-      </StyledContainer>
-    </>
-  );
+    return (
+        <>
+            <Nav />
+            <StyledContainer>
+                <CssBaseline />
+                <StyledContentWrapper>
+                    <StyledHeader>
+                        <Headline text={appInfo.headline} />
+                    </StyledHeader>
+                    {defaultValues ? (
+                        <FormWrapper defaultValues={defaultValues} />
+                    ) : (
+                        <ContainerLoader size={80} infoText="Fetching settings..." />
+                    )}
+                </StyledContentWrapper>
+            </StyledContainer>
+        </>
+    );
 };
 
 const StyledContainer = styled(Container)`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: center;
-
-  & > div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
     flex: 1;
-  }
+    align-items: center;
+
+    & > div {
+        flex: 1;
+    }
 `;
 
 const StyledContentWrapper = styled(Box)`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
 `;
 
 const StyledHeader = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 35px 16px 25px;
-  gap: 8px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 35px 16px 25px;
+    gap: 8px;
 `;
 
-const BASE_URL = appInfo.getUrl();
+const BASE_URL = appInfo.getGetUrl();
--- template/react/src/appInfo.ts
+++ template/react/src/appInfo.ts
@@ -1,10 +1,13 @@
 export const appInfo = {
-  //adjust application accodingly
-  name: "Papago",
-  packageName: "papago_temperature",
-  headline: "PAPAGO Temperature (IP device)",
-  title: "PAPAGO Temperature",
-  getUrl() {
-    return `/local/camscripter/package/settings.cgi?package_name=${this.packageName}&action=get`;
-  },
+    //adjust application accodingly
+    name: 'Papago',
+    packageName: 'papago_temperature',
+    headline: 'PAPAGO Temperature (IP device)',
+    title: 'PAPAGO Temperature',
+    getGetUrl() {
+        return `/local/camscripter/package/settings.cgi?package_name=${this.packageName}&action=get`;
+    },
+    getPostUrl() {
+        return `/local/camscripter/package/settings.cgi?package_name=${this.packageName}&action=set`;
+    },
 };
