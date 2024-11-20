60184ee056f8b8ef2a57ddcf61d0d26373e89514
Helena Beranova <helena.beranova@netrex.cz>
2024-11-20 15:38:10 +0100

    add camera_list to schema and mock

--- template/react/src/models/schema.ts
+++ template/react/src/models/schema.ts
@@ -1,35 +1,28 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const connectionParams = {
-  protocol: z.union([
-    z.literal("http"),
-    z.literal("https"),
-    z.literal("https_insecure"),
-  ]),
-  ip: z.string(),
-  port: z.number(),
-  user: z.string(),
-  pass: z.string(),
+    protocol: z.union([z.literal('http'), z.literal('https'), z.literal('https_insecure')]),
+    ip: z.string(),
+    port: z.number(),
+    user: z.string(),
+    pass: z.string(),
+    camera_list: z.array(z.number()),
 };
 export const cameraSchema = z.object({
-  ...connectionParams,
-  serviceID: z.number(),
-  fieldName: z.string(),
+    ...connectionParams,
+    serviceID: z.number(),
+    fieldName: z.string(),
 });
 export const applicationSchema = z.object({
-  protocol: z.union([
-    z.literal("http"),
-    z.literal("https"),
-    z.literal("https_insecure"),
-  ]),
-  ip: z.string(),
-  port: z.number(),
-  portID: z.string(),
-  updateFrequency: z.number(),
+    protocol: z.union([z.literal('http'), z.literal('https'), z.literal('https_insecure')]),
+    ip: z.string(),
+    port: z.number(),
+    portID: z.string(),
+    updateFrequency: z.number(),
 });
 export const settingsSchema = z.object({
-  camera: cameraSchema,
-  application: applicationSchema,
+    camera: cameraSchema,
+    application: applicationSchema,
 });
 
 export type TCamera = z.infer<typeof cameraSchema>;
