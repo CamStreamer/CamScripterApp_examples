87a9fd7992efacc38fbbc0344f5dafc4879b2a83
Helena Beranova <helena.beranova@netrex.cz>
2024-11-20 15:25:27 +0100

    add new hooks

---
+++ template/react/src/hooks/useCameraList.ts
@@ -0,0 +1,103 @@
+import { useRef, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { Path } from 'react-hook-form';
+import { TSettings } from '../models/schema';
+
+export type TCameraListOption = {
+    value: number;
+    label: string;
+};
+
+type Props = {
+    camera_list: Path<TSettings>;
+    protocol: Path<TSettings>;
+    ipAddress: Path<TSettings>;
+    port: Path<TSettings>;
+    user: Path<TSettings>;
+    pass: Path<TSettings>;
+};
+
+export const useCameraList = () => {
+    const { getValues, setValue } = useFormContext<TSettings>();
+    const [options, setOptions] = useState<TCameraListOption[]>([]);
+    const [isFetching, setIsFetching] = useState(false);
+    const fetchIdsInProgress = useRef<number[]>([]);
+    const abortControllers = useRef<AbortController | null>(null);
+
+    const fetchCameraList = async ({ camera_list, protocol, ipAddress, port, user, pass }: Props) => {
+        const fetchId = Math.round(Math.random() * 10000);
+        fetchIdsInProgress.current.push(fetchId);
+        setIsFetching(true);
+
+        if (abortControllers.current !== null) {
+            abortControllers.current.abort();
+            abortControllers.current = null;
+        }
+
+        try {
+            const controller = new AbortController();
+            abortControllers.current = controller;
+            const signal = controller.signal;
+            const res = await fetch('/local/camscripter/proxy.cgi', {
+                headers: new Headers({
+                    'x-target-camera-protocol': `${getValues(protocol)}`,
+                    'x-target-camera-ip': `${getValues(ipAddress)}`,
+                    'x-target-camera-port': `${getValues(port)}`,
+                    'x-target-camera-user': `${getValues(user)}`,
+                    'x-target-camera-pass': `${getValues(pass)}`,
+                    'x-target-camera-path': '/axis-cgi/param.cgi?action=list&group=root.Image',
+                }),
+                signal: signal,
+            });
+
+            const textRes = await res.text();
+
+            // This req is not the latest one -> cancel
+            if (fetchId !== fetchIdsInProgress.current[fetchIdsInProgress.current.length - 1]) {
+                return;
+            }
+
+            const params: Record<string, string> = {};
+            const lines = textRes.split(/[\r\n]/);
+            for (let i = 0; i < lines.length; i++) {
+                if (lines[i].length) {
+                    const p = lines[i].split('=');
+                    if (p.length >= 2) {
+                        params[p[0]] = p[1];
+                    }
+                }
+            }
+
+            const options: TCameraListOption[] = [];
+
+            let i = 0;
+            while (params[`root.Image.I${i}.Enabled`] !== undefined) {
+                if (params[`root.Image.I${i}.Source`] === 'quad') {
+                    i++;
+                    continue;
+                }
+                if (params[`root.Image.I${i}.Enabled`] === 'yes') {
+                    options.push({
+                        value: i,
+                        label: params[`root.Image.I${i}.Name`],
+                    });
+                }
+                i++;
+            }
+
+            setOptions(options);
+            setIsFetching(false);
+        } catch (e) {
+            if ((e as Error).name !== 'AbortError') {
+                setValue(camera_list, [0]);
+                setOptions([]);
+                setIsFetching(false);
+            }
+        } finally {
+            fetchIdsInProgress.current = fetchIdsInProgress.current.filter((id) => fetchId !== id);
+            abortControllers.current = null;
+        }
+    };
+
+    return [options, isFetching, fetchCameraList] as const;
+};
