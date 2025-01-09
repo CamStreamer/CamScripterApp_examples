93ea10460b0d5ab9c86a4c9cd9f75058698e16a0
Helena Beranova <helena.beranova@netrex.cz>
2025-01-09 14:56:44 +0100

    update: updated info texts

--- weighingScaleIntegration/react/src/Form/IntegrationSettings/IntegrationSettings.tsx
+++ weighingScaleIntegration/react/src/Form/IntegrationSettings/IntegrationSettings.tsx
@@ -1,12 +1,22 @@
-import { StyledRow } from './../../components/FormInputs';
+import { StyledForm, StyledRow } from './../../components/FormInputs';
 import { IntegrationDeviceSettings } from './IntegrationDeviceSettings';
 import { WidgetSettings } from './WidgetSettings';
+import { Link, FormHelperText } from '@mui/material';
 
 export const IntegrationSettings = () => {
     return (
-        <StyledRow>
-            <IntegrationDeviceSettings />
-            <WidgetSettings />
-        </StyledRow>
+        <StyledForm>
+            <FormHelperText>
+                To quickly set up the CamOverlay App, import this{' '}
+                <Link href="https://drive.google.com/file/d/11KPsROnp6mynNevVX2jDehbNXLZodolN/view" target="_blank">
+                    file
+                </Link>{' '}
+                into the CamOverlay App in the Settings section.
+            </FormHelperText>
+            <StyledRow>
+                <IntegrationDeviceSettings />
+                <WidgetSettings />
+            </StyledRow>
+        </StyledForm>
     );
 };
--- weighingScaleIntegration/react/src/Form/StationSettings/StationSettings.tsx
+++ weighingScaleIntegration/react/src/Form/StationSettings/StationSettings.tsx
@@ -1,12 +1,29 @@
-import { StyledRow } from '../../components/FormInputs';
+import { StyledForm, StyledRow } from '../../components/FormInputs';
 import { StationEventTrigger } from './StationEventTrigger';
 import { CameraStationSettings } from './CameraStationSettings';
+import { FormHelperText, Link } from '@mui/material';
 
 export const StationSettings = () => {
     return (
-        <StyledRow>
-            <CameraStationSettings />
-            <StationEventTrigger />
-        </StyledRow>
+        <StyledForm>
+            <FormHelperText>
+                To run external data on the Axis Camera Station (ACS), you need to edit the registry on the computer
+                where ACS is installed using{' '}
+                <Link href="https://drive.google.com/file/d/176uMtC1goF4kBEFtx0IBMrZ5coo_zPeN/view" target="_blank">
+                    this
+                </Link>{' '}
+                guide (or use the provided{' '}
+                <Link href="https://camstreamer.com/acsregistery" target="_blank">
+                    file
+                </Link>
+                ). Then, create a new source in Devices &#62; External data sources. Use the generated Source key in the
+                settings below. To search for external data in ACS, click the + button, select &#39;Data search&#39;,
+                and set the first filter to the value &#39;Live&#39;.
+            </FormHelperText>
+            <StyledRow>
+                <CameraStationSettings />
+                <StationEventTrigger />
+            </StyledRow>
+        </StyledForm>
     );
 };
