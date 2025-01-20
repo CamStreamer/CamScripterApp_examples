5be3dc08e11a4a6aebadc84d51fdd956eb897c09
Helena Beranova <helena.beranova@netrex.cz>
2025-01-20 14:24:43 +0100

    update: added info texts to Axis Camera Station and CamOverlay settings

--- temper1fSensor/react/src/Form/IntegrationSettings/IntegrationSettings.tsx
+++ temper1fSensor/react/src/Form/IntegrationSettings/IntegrationSettings.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, FormHelperText, Link } from '@mui/material';
 import styled from '@mui/material/styles/styled';
 import { IntegrationDeviceSettings } from './IntegrationDeviceSettings';
 import { WidgetSettings } from './WidgetSettings';
@@ -6,6 +6,16 @@ import { WidgetSettings } from './WidgetSettings';
 export const IntegrationSettings = () => {
     return (
         <StyledGrid container>
+            <StyledFormHelperText>
+                To quickly set up the CamOverlay App, import this{' '}
+                <Link
+                    href="https://drive.google.com/file/d/11egz7D8l6KSZOO6XHZtcHBrEGZhKt3RG/view?usp=sharing"
+                    target="_blank"
+                >
+                    file
+                </Link>{' '}
+                into the CamOverlay App in the Settings section.
+            </StyledFormHelperText>
             <Grid item md={6} xs={12}>
                 <IntegrationDeviceSettings />
             </Grid>
@@ -18,4 +28,8 @@ export const IntegrationSettings = () => {
 
 const StyledGrid = styled(Grid)`
     margin-bottom: 15px;
+`;
+
+const StyledFormHelperText = styled(FormHelperText)`
+    margin-bottom: 8px;
 `;
--- temper1fSensor/react/src/Form/StationSettings/StationSettings.tsx
+++ temper1fSensor/react/src/Form/StationSettings/StationSettings.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, FormHelperText, Link } from '@mui/material';
 import styled from '@mui/material/styles/styled';
 import { StationEventTrigger } from './StationEventTrigger';
 import { CameraStationSettings } from './CameraStationSettings';
@@ -6,6 +6,20 @@ import { CameraStationSettings } from './CameraStationSettings';
 export const StationSettings = () => {
     return (
         <StyledGrid container>
+            <StyledFormHelperText>
+                To run external data on the Axis Camera Station (ACS), you need to edit the registry on the computer
+                where ACS is installed using{' '}
+                <Link href="https://drive.google.com/file/d/176uMtC1goF4kBEFtx0IBMrZ5coo_zPeN/view" target="_blank">
+                    this
+                </Link>{' '}
+                guide (or use the provided{' '}
+                <Link href="https://drive.google.com/file/d/10Novidd_ZpbYUFQUtcV1RLXKUxyPFg-n/view" target="_blank">
+                    file
+                </Link>
+                ). Then, create a new source in Devices &#62; External data sources. Use the generated Source key in the
+                settings below. To search for external data in ACS, click the + button, select &#39;Data search&#39;,
+                and set the first filter to the value &#39;Live&#39;.
+            </StyledFormHelperText>
             <Grid item md={6} xs={12}>
                 <CameraStationSettings />
             </Grid>
@@ -18,4 +32,8 @@ export const StationSettings = () => {
 
 const StyledGrid = styled(Grid)`
     margin-bottom: 15px;
+`;
+
+const StyledFormHelperText = styled(FormHelperText)`
+    margin-bottom: 8px;
 `;
