814f87bc7d8f6cc99755c11e18249a3c18df6400
Helena Beranova <helena.beranova@netrex.cz>
2025-02-19 20:04:22 +0100

    update: new method in genetec for fetching camera list

--- videoCheckpoint/src/upload/Genetec.ts
+++ videoCheckpoint/src/upload/Genetec.ts
@@ -2,17 +2,21 @@ import { TServerData } from '../schema';
 import { pad } from '../utils';
 
 const ACTION = 'AddCameraBookmark';
+const GET_CAMERAS_URL = 'report/EntityConfiguration?q=EntityTypes@Camera';
+const GET_CAMERAS_DETAILS_URL = '/entity?q=';
+const PARAMS = 'Guid,Name,EntityType';
 
 export class Genetec {
     private baseUrl: string;
     private credentials: string;
 
     constructor(private genetecSettings: TServerData['genetec']) {
-        this.baseUrl = `${genetecSettings.protocol}://${genetecSettings.ip}:${genetecSettings.port}/${genetecSettings.base_uri}/action?q=`;
-        this.credentials = `${genetecSettings.user};${genetecSettings.app_id}:${genetecSettings.pass}`;
+        this.baseUrl = `${genetecSettings.protocol}://${genetecSettings.ip}:${genetecSettings.port}/${genetecSettings.base_uri}`;
+        this.credentials = btoa(`${genetecSettings.user};${genetecSettings.app_id}:${genetecSettings.pass}`);
     }
 
     async sendBookmark(code: string) {
+        console.log('Sending bookmark...');
         const date = new Date();
         const year = date.getUTCFullYear();
         const month = pad(date.getUTCMonth() + 1, 2);
@@ -23,7 +27,6 @@ export class Genetec {
         const miliSeconds = pad(date.getUTCMilliseconds(), 2);
 
         const timeStamp = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${miliSeconds}Z`;
-        const encodedCredentials = btoa(this.credentials);
         const bookmarkText = code;
         const cameraEntitiesUrl = [];
 
@@ -31,21 +34,76 @@ export class Genetec {
             cameraEntitiesUrl.push(`${ACTION}(${camera},${timeStamp},${bookmarkText})`);
         }
 
-        const requestOptions: RequestInit = {
-            method: 'POST',
+        const requestOptions = this.requestOptionsCreator('POST');
+
+        try {
+            fetch(`${this.baseUrl}/action?q=${cameraEntitiesUrl.join(',')}`, requestOptions).catch((error) =>
+                console.error(error)
+            );
+            console.log('Bookmark sent');
+        } catch (err) {
+            throw new Error('Error sending bookmark' + err);
+        }
+    }
+
+    async getCameraList() {
+        try {
+            const guidsArray = await this.getAllCamerasGuids().then((res) => res.Rsp.Result);
+            const camerasGuids = guidsArray.map((guid: { Guid: string }) => guid.Guid);
+            const camerasDetailsUrl = [];
+
+            for (const guid of camerasGuids) {
+                camerasDetailsUrl.push(`entity=${guid},${PARAMS}`);
+            }
+
+            const requestOptions = this.requestOptionsCreator('GET');
+
+            const camerasDetails = await fetch(
+                `${this.baseUrl}/${GET_CAMERAS_DETAILS_URL}${camerasDetailsUrl.join(',')}`,
+                requestOptions
+            )
+                .then((res) => res.json())
+                .then((response) => response.Rsp.Result)
+                .catch((error) => {
+                    console.error(error);
+                });
+
+            const cameraList = [];
+
+            if (!Array.isArray(camerasDetails)) {
+                cameraList.push({
+                    value: camerasDetails.Guid,
+                    label: camerasDetails.Name,
+                });
+            } else {
+                for (const camera of camerasDetails) {
+                    cameraList.push({
+                        value: camera.Guid,
+                        label: camera.Name,
+                    });
+                }
+            }
+            console.log('Camera list:', cameraList);
+            return cameraList;
+        } catch (e) {
+            console.error(e);
+            return [];
+        }
+    }
+
+    private async getAllCamerasGuids() {
+        const requestOptions = this.requestOptionsCreator('GET');
+        return await fetch(`${this.baseUrl}/${GET_CAMERAS_URL}`, requestOptions).then((res) => res.json());
+    }
+
+    private requestOptionsCreator(method: string): RequestInit {
+        return {
+            method: method,
             headers: new Headers({
-                Authorization: `Basic ${encodedCredentials}`,
+                Authorization: `Basic ${this.credentials}`,
                 Accept: 'text/json',
             }),
             redirect: 'follow' as RequestRedirect,
         };
-
-        try {
-            fetch(`${this.baseUrl}${cameraEntitiesUrl.join(',')}`, requestOptions).catch((error) =>
-                console.error(error)
-            );
-        } catch (err) {
-            throw new Error('Error sending bookmark' + err);
-        }
     }
 }
