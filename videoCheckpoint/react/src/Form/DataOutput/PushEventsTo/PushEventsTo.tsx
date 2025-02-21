9ee4ea1d735d856263651966177ec120095bedf6
Helena Beranova <helena.beranova@netrex.cz>
2025-02-21 16:35:09 +0100

    add: missing settings param, text update

--- videoCheckpoint/react/src/Form/DataOutput/PushEventsTo/PushEventsTo.tsx
+++ videoCheckpoint/react/src/Form/DataOutput/PushEventsTo/PushEventsTo.tsx
@@ -87,7 +87,7 @@ export const PushEventsTo = () => {
                                 }}
                             />
                         }
-                        label={'Genetec'}
+                        label={'Genetec VMS'}
                     />
                 )}
             />
