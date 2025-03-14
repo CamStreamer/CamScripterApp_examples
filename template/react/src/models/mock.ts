a76417a6a127848c287fdd155011638f3137d9aa
Helena Beranova <helena.beranova@netrex.cz>
2025-03-14 12:44:33 +0100

    update: schema structure

--- template/react/src/models/mock.ts
+++ template/react/src/models/mock.ts
@@ -2,21 +2,22 @@ import { TSettings } from './schema';
 
 export const mockedSettings: TSettings = {
     //adjust application accodingly
-    application: {
-        protocol: 'http',
-        ip: '192.168.91.247',
-        port: 80,
-        portID: '1',
-        updateFrequency: 1,
-    },
     camera: {
         protocol: 'http',
         ip: '127.0.0.1',
         port: 80,
         user: 'root',
         pass: '',
-        camera_list: [0],
-        serviceID: 1,
-        fieldName: '',
+        update_frequency: 0,
+        port_id: '1',
+    },
+    output_camera: {
+        protocol: 'http',
+        ip: '127.0.0.1',
+        port: 80,
+        user: 'root',
+        pass: '',
+        field_name: '1',
+        service_id: '1',
     },
 };
